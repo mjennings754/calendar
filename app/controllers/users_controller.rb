7df1641d34ca41daa600a9e3adea705bca4bf3d5
mjennings754 <mitchellj@outlook.jp>
2026-02-12 09:21:52 +1100

    can add users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update destroy]
+  def index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy!
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.expect(user: [:username, :email, :password, :password_confirmation])
+  end
+end
