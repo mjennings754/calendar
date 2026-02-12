f1fcd7b88efd7d2e1737bc185aa6d056a6038b68
mjennings754 <mitchellj@outlook.jp>
2026-02-13 08:06:44 +1100

    login user after account creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       UserMailer.email_verification(@user).deliver_later
-      redirect_to @user
+      login(@user)
+      redirect_to dashboard_path
     else
       render :new
     end
